4dc6ecdbc76d050bf1460815ad3238ddb6a51dc4
Thales Rogério <thalesrogerio@gmail.com>
2022-06-21 16:02:32 -0300

    making necessary changes for compiling

--- 02-build-lammps.sh
+++ 02-build-lammps.sh
@@ -6,8 +6,13 @@ function fail() {
 
 mkdir build || fail "Could not create the build directory"
 
+docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/utils ubuntu:mpi-dev make
+
 docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/build ubuntu:mpi-dev cmake ../cmake/ -DCMAKE_BUILD_TYPE=Release  -DPKG_KSPACE=on -DPKG_MANYBODY=on -DPKG_RIGID=on -DPKG_MISC=on -DPKG_MOLECULE=on -DBUILD_MPI=on -DPKG_GRANULAR=on -DMPI_C_COMPILER=mpicc -DMPI_CXX_COMPILER=mpic++ -DBUILD_OMP=on  || \
 	fail "Error when configuring LAMMPS build directory" 
 	
 docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/build ubuntu:mpi-dev make -j 4 || \
 	fail "Error when building LAMMPS"
+
+# build utils
+#docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd) ubuntu:mpi-dev make
