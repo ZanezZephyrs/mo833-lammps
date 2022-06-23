2e4a7354657833de417ce7d58eaaeb11190dc581
Thales Rogério <thalesrogerio@gmail.com>
2022-06-23 15:37:06 -0300

    update run scripts with new parameters, allow custom data

--- 03-run-simple-LAMMPS-workload-noMPI.sh
+++ 03-run-simple-LAMMPS-workload-noMPI.sh
@@ -4,7 +4,12 @@ function fail() {
 }
 
 # Run command inside docker at directory $(pwd)/bench
-docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/bench ubuntu:mpi-dev \
-	../build/lmp -in in.spce 1> in.spce.results.stdout 2> in.spce.results.stderr || \
+INPUT_FILE=$1
+MAX_LAMMPS_STEPS=$2
+STDOUT_FILE=proxy_full_out
+FINAL_TIME_FILE=proxy_time.txt
+docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/bench -e MAX_LAMMPS_STEPS=$MAX_LAMMPS_STEPS ubuntu:mpi-dev \
+	../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.stderr || \
 	fail "Error when executing LAMMPS"
 	
+cat $STDOUT_FILE | grep "Loop time of" | cut -d " " -f 4 > $FINAL_TIME_FILE
--- 04-run-simple-LAMMPS-workload-MPI-localhost.sh
+++ 04-run-simple-LAMMPS-workload-MPI-localhost.sh
@@ -3,8 +3,13 @@ function fail() {
   exit 1
 }
 
+#in.spce
+INPUT_FILE=$1
+STDOUT_FILE=proxy_full_out
+FINAL_TIME_FILE=full_time.txt
 # Run command inside docker at directory $(pwd)/bench
 docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/bench ubuntu:mpi-dev \
-	mpirun -np 2 -host localhost,localhost ../build/lmp -in in.spce 1> in.spce.results.N.2.stdout 2> in.spce.results.N.2.stderr || \
+	mpirun -np 2 -host "localhost,localhost" ../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.N.2.stderr || \
 	fail "Error when executing LAMMPS"
 	
+cat $STDOUT_FILE | grep "Loop time of" | cut -d " " -f 4 > $FINAL_TIME_FILE
