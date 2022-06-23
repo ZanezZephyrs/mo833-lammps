3f4a3750d01a7ae7d5d98016eb6a4de7dbce2f19
Thales Rogério <thalesrogerio@gmail.com>
2022-06-23 15:46:58 -0300

    add number of nodes and hosts as parameter to run scripts

--- 03-run-simple-LAMMPS-workload-noMPI.sh
+++ 03-run-simple-LAMMPS-workload-noMPI.sh
@@ -5,11 +5,13 @@ function fail() {
 
 # Run command inside docker at directory $(pwd)/bench
 INPUT_FILE=$1
-MAX_LAMMPS_STEPS=$2
+NUMBER_OF_NODES=$2
+HOSTS=$3
+MAX_LAMMPS_STEPS=$4
 STDOUT_FILE=proxy_full_out
 FINAL_TIME_FILE=proxy_time.txt
 docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/bench -e MAX_LAMMPS_STEPS=$MAX_LAMMPS_STEPS ubuntu:mpi-dev \
-	../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.stderr || \
+	-np $NUMBER_OF_NODES -host $HOSTS ../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.stderr || \
 	fail "Error when executing LAMMPS"
 	
 cat $STDOUT_FILE | grep "Loop time of" | cut -d " " -f 4 > $FINAL_TIME_FILE
