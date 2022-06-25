e16d3a413a62a4de46f10626393ea02ceef618b9
Thales Rogério <thalesrogerio@gmail.com>
2022-06-25 16:52:58 -0300

    changes to use hostfiles

--- 03-run-simple-LAMMPS-workload-noMPI.sh
+++ 03-run-simple-LAMMPS-workload-noMPI.sh
@@ -11,7 +11,7 @@ MAX_LAMMPS_STEPS=$4
 STDOUT_FILE=proxy_full_out
 FINAL_TIME_FILE=proxy_time.txt
 docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/bench -e MAX_LAMMPS_STEPS=$MAX_LAMMPS_STEPS ubuntu:mpi-dev \
-	-np $NUMBER_OF_NODES -host $HOSTS ../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.stderr || \
+	mpirun -np $NUMBER_OF_NODES --hostfile $HOSTS ../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.stderr || \
 	fail "Error when executing LAMMPS"
 	
 cat $STDOUT_FILE | grep "Loop time of" | cut -d " " -f 4 > $FINAL_TIME_FILE
--- 04-run-simple-LAMMPS-workload-MPI-localhost.sh
+++ 04-run-simple-LAMMPS-workload-MPI-localhost.sh
@@ -12,7 +12,7 @@ STDOUT_FILE=proxy_full_out
 FINAL_TIME_FILE=full_time.txt
 # Run command inside docker at directory $(pwd)/bench
 docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/bench ubuntu:mpi-dev \
-	mpirun -np $NUMBER_OF_NODES -host $HOSTS ../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.N.2.stderr || \
+	mpirun -np $NUMBER_OF_NODES --hostfile $HOSTS ../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.N.2.stderr || \
 	fail "Error when executing LAMMPS"
 	
 cat $STDOUT_FILE | grep "Loop time of" | cut -d " " -f 4 > $FINAL_TIME_FILE
