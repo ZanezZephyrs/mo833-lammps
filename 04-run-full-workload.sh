e123ba2d222edd2710a6b51ccae30b5ca1234054
Thales Rogério <thalesrogerio@gmail.com>
2022-06-25 18:01:42 -0300

    change name of run scripts, change output time file to time.txt

--- 04-run-full-workload.sh
+++ 04-run-full-workload.sh
@@ -8,8 +8,8 @@ INPUT_FILE=$1
 #"localhost,localhost"
 NUMBER_OF_NODES=$2
 HOSTS=$3
-STDOUT_FILE=proxy_full_out
-FINAL_TIME_FILE=full_time.txt
+STDOUT_FILE=full_run_full_out
+FINAL_TIME_FILE=time.txt
 # Run command inside docker at directory $(pwd)/bench
 docker run --rm --user=$(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd)/bench ubuntu:mpi-dev \
 	mpirun -np $NUMBER_OF_NODES --hostfile $HOSTS ../build/lmp -in $INPUT_FILE 1> $STDOUT_FILE 2> in.spce.results.N.2.stderr || \
